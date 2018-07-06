a18af19c7d901fb7111dc12cc407d1f4b12e5171
Paulo Alves <juninhpaulo@hotmail.com>
2018-07-06 19:29:07 -0300

    update login

--- app/src/main/java/com/pstwh/uepgacadonline_android/MainActivity.java
+++ app/src/main/java/com/pstwh/uepgacadonline_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.pstwh.uepgacadonline_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +12,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Scrapper scrapper = new Scrapper("14147326", "");
-        scrapper.getGrade();
-
-
         setContentView(R.layout.activity_main);
+
+        final EditText ra = (EditText) findViewById(R.id.ra_edit_text);
+        final EditText password = (EditText) findViewById(R.id.password_edit_text);
+
+
+        final Button login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Scrapper scrapper = new Scrapper(ra.getText().toString(), password.getText().toString());
+                scrapper.getGrade();
+            }
+        });
     }
 }
