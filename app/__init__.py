c98eab0448007ce7b6c10976a43c1be734ec1a76
Paulo Alves <juninhpaulo@hotmail.com>
2018-09-16 10:21:01 -0300

    adding random index return

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,11 @@ from app.pergamum import pergamum
 
 app = Flask(__name__)
 
+
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>uepg-acadonline api</h1>"
+
+
 app.register_blueprint(acadonline)
 app.register_blueprint(pergamum)
