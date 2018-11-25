22a641479b05a4f797d57e404b00f967d6fede7d
Paulo Alves <juninhpaulo@hotmail.com>
2018-11-25 13:33:49 -0200

    Update: Perfil Model

--- app/models/perfil.py
+++ app/models/perfil.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+import string
+import datetime
 
+from dataclasses import dataclass
 
 @dataclass
 class Perfil:
@@ -8,7 +10,7 @@ class Perfil:
     social_name: str
     genre: str
     social_genre: str
-    birth_date: str
+    birth_date: datetime.datetime
     nacionality: str
     birth_state: str
     birth_city: str
@@ -17,7 +19,7 @@ class Perfil:
     mother_name: str
     cep: str
     address: str
-    address_number: str
+    address_number: int
     complement: str
     neighborhood: str
     city: str
@@ -35,11 +37,61 @@ class Perfil:
     category: str
     agente: str
     date: str
-    degree: str
+    degree: int
     type: str
-    year: str
+    year: int
     instituition: str
     course: str
     instituition_country: str
     instituition_state: str
     institution_city: str
+    first_name: str = ""
+
+    def __post_init__(self):
+        if type(self.address_number) is str:
+            self.address_number = int(self.address_number)
+
+        if type(self.degree) is str:
+            self.degree = int(self.degree)
+
+        if type(self.year) is str:
+            year = self.year.replace(",", "")
+            year = int(year)
+
+            self.year = year
+
+        if type(self.telephone) is str:
+            ddd = self.telephone[0:2]
+            telephone = self.telephone[2:].strip()
+
+            self.telephone = ddd + telephone
+
+        cap_word_fields = [
+            "birth_city",
+            "birth_state",
+            "birth_country",
+            "city",
+            "complement",
+            "complete_name",
+            "course",
+            "document_state",
+            "father_name",
+            "instituition",
+            "institution_city",
+            "instituition_state",
+            "instituition_country",
+            "mother_name",
+            "nacionality",
+            "neighborhood",
+            "neighborhood"
+        ]
+
+        for cap_word_field in cap_word_fields:
+            setattr(
+                self, cap_word_field, string.capwords(
+                    getattr(self, cap_word_field)
+                )
+            )
+
+        self.first_name = self.complete_name.partition(' ')[0]
+        self.birth_date = datetime.datetime.strptime(self.birth_date, '%d/%m/%Y')
