bf737fded4ae682125bd9485542d526365b682f9
Paulo Alves <juninhpaulo@hotmail.com>
2018-12-03 19:55:47 -0200

    Update: urls

--- utils/urls.py
+++ utils/urls.py
@@ -1,16 +1,16 @@
 acadonline_urls = {
-    "home": "https://sistemas.uepg.br/academicoonline/login/index",
-    "auth": "https://sistemas.uepg.br/academicoonline/login/authenticate",
-    "grades": "https://sistemas.uepg.br/academicoonline/avaliacaoDesempenho/index",
-    "password": "https://sistemas.uepg.br/academicoonline/academico/index",
-    "perfil_get": "https://sistemas.uepg.br/academicoonline/academico_pessoa/show",
-    "perfil_set": "https://sistemas.uepg.br/academicoonline/academico_pessoa/index",
-    "activities": "https://sistemas.uepg.br/academicoonline/atividadeComplementar/index",
-    "remember_password": "https://sistemas.uepg.br/academicoonline/recuperarSenha/index",
+    "home": "http://sistemas.uepg.br/academicoonline/login/index",
+    "auth": "http://sistemas.uepg.br/academicoonline/login/authenticate",
+    "grades": "http://sistemas.uepg.br/academicoonline/avaliacaoDesempenho/index",
+    "password": "http://sistemas.uepg.br/academicoonline/academico/index",
+    "perfil_get": "http://sistemas.uepg.br/academicoonline/academico_pessoa/show",
+    "perfil_set": "http://sistemas.uepg.br/academicoonline/academico_pessoa/index",
+    "activities": "http://sistemas.uepg.br/academicoonline/atividadeComplementar/index",
+    "remember_password": "http://sistemas.uepg.br/academicoonline/recuperarSenha/index",
 }
 
 pergamum_urls = {
-    "home": "https://sistemas.uepg.br/pergamum/biblioteca_s/meu_pergamum/index.php?flag=index.php",
-    "auth": "https://sistemas.uepg.br/pergamum///biblioteca/index.php",
-    "search": "https://sistemas.uepg.br/pergamum///biblioteca/index.php",
+    "home": "http://sistemas.uepg.br/pergamum/biblioteca_s/meu_pergamum/index.php?flag=index.php",
+    "auth": "http://sistemas.uepg.br/pergamum///biblioteca/index.php",
+    "search": "http://sistemas.uepg.br/pergamum///biblioteca/index.php",
 }
