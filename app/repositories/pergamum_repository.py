2164ec60105e3895458e8bee93310c70f234ad8a
Paulo Alves <juninhpaulo@hotmail.com>
2018-12-12 23:30:00 -0200

    Update: Pergamum book mock

--- app/repositories/pergamum_repository.py
+++ app/repositories/pergamum_repository.py
@@ -28,6 +28,26 @@ def books_mock():
             "name": "Computação quântica e informação quântica / 2005 - Livros",
             "renew": 127440,
             "title": "NIELSEN, Michael A. <b> Computação quântica e informação quântica. </b>  Porto Alegre: Bookman, 2005. 733 p. ISBN 85-363-0554-1. "
+        },
+        {
+            "address": "https://sistemas.uepg.br/pergamum/biblioteca/index.php?codAcervo=193457",
+            "currentRenewalsNumber": 2,
+            "fine": "0",
+            "limitDate": "Fri, 14 Dec 2018 00:00:00 GMT",
+            "maxRenewalNumber": 6,
+            "name": "Computação quântica e informação quântica / 2005 - Livros",
+            "renew": 127440,
+            "title": "NIELSEN, Michael A. <b> Computação quântica e informação quântica. </b>  Porto Alegre: Bookman, 2005. 733 p. ISBN 85-363-0554-1. "
+        },
+        {
+            "address": "https://sistemas.uepg.br/pergamum/biblioteca/index.php?codAcervo=193457",
+            "currentRenewalsNumber": 2,
+            "fine": "0",
+            "limitDate": "Fri, 14 Dec 2018 00:00:00 GMT",
+            "maxRenewalNumber": 6,
+            "name": "Computação quântica e informação quântica / 2005 - Livros",
+            "renew": 127440,
+            "title": "NIELSEN, Michael A. <b> Computação quântica e informação quântica. </b>  Porto Alegre: Bookman, 2005. 733 p. ISBN 85-363-0554-1. "
         }
     ]
 
