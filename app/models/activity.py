fae4fd492de2c3c48a471429b0bd6bc4e82f8bfe
Paulo Alves <paulo@esfera.com.br>
2019-05-08 23:45:49 -0300

    Fix: Formato Data Atividade

--- app/models/activity.py
+++ app/models/activity.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 
+import datetime
 
 @dataclass
 class Activity:
     protocol: str
-    date: str
+    date: datetime.datetime
     hours: str
     minutes: str
+
+    def __post_init__(self):
+        self.date = datetime.datetime.strptime(self.date, '%d/%m/%Y')
