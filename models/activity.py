2303be5ff8789d87355253939c671e22d99561c7
Paulo Alves <paulo@esfera.com.br>
2019-05-12 05:34:33 -0300

    Update: Texto Protocólo

--- models/activity.py
+++ models/activity.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 import datetime
 
+
 @dataclass
 class Activity:
     protocol: str
@@ -11,3 +12,4 @@ class Activity:
 
     def __post_init__(self):
         self.date = datetime.datetime.strptime(self.date, '%d/%m/%Y')
+        self.protocol = self.protocol[13:]
