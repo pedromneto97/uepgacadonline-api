6e92eb8582c4ed6b29692eb9449ca06806e11bae
Pedro Neto <pedromneto97@gmail.com>
2019-05-13 18:02:37 -0300

    Add: Blueprint dos eventos

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from blueprints.acadonline_blueprint import acadonline_blueprint
+from blueprints.eventos_blueprint import eventos_blueprint
 from blueprints.pergamum_blueprint import pergamum_blueprint
 from blueprints.ru_blueprint import ru_blueprint
 from blueprints.portal_blueprint import portal_blueprint
@@ -19,3 +20,4 @@ app.register_blueprint(acadonline_blueprint)
 app.register_blueprint(pergamum_blueprint)
 app.register_blueprint(ru_blueprint)
 app.register_blueprint(portal_blueprint)
+app.register_blueprint(eventos_blueprint)
